2c8ba6e1e9a40ed7e7502ce40f074abd8efe114a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-23 14:39:24 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/cvfeedbackaichatbotapp/CvFeedbackAiChatbotAppApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.cvfeedbackaichatbotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvFeedbackAiChatbotAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvFeedbackAiChatbotAppApplication.class, args);
+    }
+
+}
