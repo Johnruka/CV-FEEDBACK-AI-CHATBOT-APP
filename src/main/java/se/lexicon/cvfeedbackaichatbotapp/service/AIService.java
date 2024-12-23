47631f2033bfd0071979d9b414b3f077398baa9d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-23 17:10:48 +0100

    Created and added methods

---
+++ src/main/java/se/lexicon/cvfeedbackaichatbotapp/service/AIService.java
@@ -0,0 +1,14 @@
+package se.lexicon.cvfeedbackaichatbotapp.service;
+
+import se.lexicon.entity.CVFeedbackChatbot;
+
+public interface AIService {
+
+    public String generateFeedback(CVFeedbackChatbot cvFeedbackChatbot);
+
+    String convertCVFeedbackChatbotToText(CVFeedbackChatbot cvFeedbackChatbot);
+
+    String callAIModelApi(String inputText);
+
+    }
+
