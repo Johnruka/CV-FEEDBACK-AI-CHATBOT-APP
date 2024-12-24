f3d81266d433bdb829d9f8321939d1a8a0ba55db
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-24 11:59:51 +0100

    deleted

--- src/main/java/se/lexicon/cvfeedbackaichatbotapp/AIRepository.java
+++
@@ -1,7 +0,0 @@
-package se.lexicon.cvfeedbackaichatbotapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.lexicon.entity.CVFeedbackChatbot;
-
-public interface AIRepository extends JpaRepository<CVFeedbackChatbot, Long> {
-}
